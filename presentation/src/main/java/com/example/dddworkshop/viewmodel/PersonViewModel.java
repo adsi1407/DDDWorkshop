3d3c63497a1347c067c06650617fdd7dfaf45dbc
David Santacoloma <adsi1407@gmail.com>
2020-08-28 04:47:02 -0500

    Implementar ViewModel en Android

---
+++ presentation/src/main/java/com/example/dddworkshop/viewmodel/PersonViewModel.java
@@ -0,0 +1,60 @@
+package com.example.dddworkshop.viewmodel;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import androidx.hilt.lifecycle.ViewModelInject;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.domain.aggregate.Person;
+import com.example.domain.service.PersonService;
+
+public class PersonViewModel extends ViewModel {
+
+    private MutableLiveData<Boolean> personSaved;
+
+    private final PersonService personService;
+
+    @ViewModelInject
+    public PersonViewModel(PersonService personService) {
+        this.personService = personService;
+    }
+
+    public LiveData<Boolean> executeSavePerson(Person person) {
+
+        if (personSaved == null) {
+            personSaved = new MutableLiveData<>();
+            savePerson(person);
+        }
+        return personSaved;
+    }
+
+    private void savePerson(Person person) {
+
+        PersonAsyncTask personAsyncTask = new PersonAsyncTask();
+        personAsyncTask.execute(person);
+    }
+
+    class PersonAsyncTask extends AsyncTask<Person, String, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Person... people) {
+
+            try {
+                personService.savePerson(people[0]);
+                return true;
+            }
+            catch (Exception ex) {
+                Log.println(Log.ERROR, PersonViewModel.class.getName(), ex.getMessage());
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean saved) {
+            personSaved.setValue(saved);
+        }
+    }
+}
