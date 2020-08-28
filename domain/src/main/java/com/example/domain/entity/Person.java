8d4fd419f78191598847879c59b5ef2e092f0841
David Santacoloma <adsi1407@gmail.com>
2020-08-27 21:58:44 -0500

    Agregar propiedades mencionadas en el problema de negocio a la clase persona

--- domain/src/main/java/com/example/domain/entity/Person.java
+++ domain/src/main/java/com/example/domain/entity/Person.java
@@ -1,4 +1,20 @@
 package com.example.domain.entity;
 
+import java.util.Date;
+import java.util.List;
+
 public class Person {
+
+    private String id;
+    private String name;
+    private Date birthday;
+    private String phone;
+    private String email;
+    private String address;
+    private String city;
+    private String latitude;
+    private String longitude;
+    private List<String> roles;
+    private String userID;
+    private String userPass;
 }
