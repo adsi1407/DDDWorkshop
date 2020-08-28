98c15d9987549ef5d43a39afba014d3503decddf
David Santacoloma <adsi1407@gmail.com>
2020-08-27 23:13:14 -0500

    Agregar lógica en el servicio de dominio para cumplir con los requerimientos del negocio

--- domain/src/main/java/com/example/domain/service/PersonService.java
+++ domain/src/main/java/com/example/domain/service/PersonService.java
@@ -1,4 +1,23 @@
 package com.example.domain.service;
 
+import com.example.domain.entity.Person;
+import com.example.domain.repository.PersonRepository;
+
 public class PersonService {
+
+    private PersonRepository personRepository;
+
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public void savePerson(Person person) {
+
+        if (personRepository.personExists(person.getId())) {
+            //throws exception
+        }
+        else {
+            personRepository.savePerson(person);
+        }
+    }
 }
