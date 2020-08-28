87947c7e31e906783da75565477c89cb62cfac22
David Santacoloma <adsi1407@gmail.com>
2020-08-27 23:03:13 -0500

    Definir métodos en el repositorio para cumplir con los requerimientos del negocio

--- domain/src/main/java/com/example/domain/repository/PersonRepository.java
+++ domain/src/main/java/com/example/domain/repository/PersonRepository.java
@@ -1,4 +1,10 @@
 package com.example.domain.repository;
 
+import com.example.domain.entity.Person;
+
 public interface PersonRepository {
+
+    boolean personExists(String id);
+
+    void savePerson(Person person);
 }
