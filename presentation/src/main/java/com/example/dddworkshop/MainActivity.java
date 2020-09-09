3a487c722cfdb18f89f4afbd30a4aa3af95ab5b9
David Santacoloma <adsi1407@gmail.com>
2020-09-08 22:05:16 -0500

    Inyectar servicio de dominio en la capa de presentación

--- presentation/src/main/java/com/example/dddworkshop/MainActivity.java
+++ presentation/src/main/java/com/example/dddworkshop/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.domain.aggregate.Person;
+import com.example.domain.entity.Role;
+import com.example.domain.service.CreatePersonService;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    CreatePersonService createPersonService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Person person = new Person("01", "David", "3001112233", new Role("01", "Role01"));
+        createPersonService.savePerson(person);
     }
 }
