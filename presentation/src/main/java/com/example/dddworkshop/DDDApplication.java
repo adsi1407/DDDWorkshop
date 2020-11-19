4ad5c84b25835f37134abc873e2c7cf5c8242352
David Santacoloma <adsi1407@gmail.com>
2020-11-19 07:50:11 -0500

    Agregar clase de tipo Application a la capa de presentación

---
+++ presentation/src/main/java/com/example/dddworkshop/DDDApplication.java
@@ -0,0 +1,9 @@
+package com.example.dddworkshop;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class DDDApplication extends Application {
+}
