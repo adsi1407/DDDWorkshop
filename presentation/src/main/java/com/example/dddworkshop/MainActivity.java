a2dd34c56ee4fbd4e69310772c2b60ae101ae96d
David Santacoloma <adsi1407@gmail.com>
2020-11-19 07:52:28 -0500

    Llamar al dominio desde la capa de presentación

--- presentation/src/main/java/com/example/dddworkshop/MainActivity.java
+++ presentation/src/main/java/com/example/dddworkshop/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.dataaccess.repository.PersonRepositoryWebService;
+import com.example.domain.model.AuthUser;
+import com.example.domain.model.ContactInformation;
+import com.example.domain.model.Person;
+import com.example.domain.repository.PersonRepository;
+import com.example.domain.service.PersonService;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    PersonService personService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ContactInformation contactInformation = new ContactInformation("3121110011");
+        Person person = new Person("01", "David", contactInformation, new AuthUser());
+        personService.savePerson(person);
+
+
     }
 }
